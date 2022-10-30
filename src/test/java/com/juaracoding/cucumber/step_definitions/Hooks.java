e474fc7b60d877c4e4bd39ac384bbf12fd2d8eed
Mohammad Zakaria <zakariayus10@gmail.com>
2022-10-30 13:45:51 +0700

    fix JDK cucumber hooks commit

--- src/test/java/com/juaracoding/cucumber/step_definitions/Hooks.java
+++ src/test/java/com/juaracoding/cucumber/step_definitions/Hooks.java
@@ -6,14 +6,15 @@ import com.juaracoding.cucumber.utils.TestScenarios;
 import com.juaracoding.cucumber.utils.Utils;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.Before;
+import com.relevantcodes.extentreports.LogStatus;
+import io.cucumber.java.*;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
+import java.io.IOException;
 
 public class Hooks {
+
     public static WebDriver driver;
     public static ExtentTest extentTest;
     public static ExtentReports reports = new ExtentReports("target/extent-reports.html");
@@ -27,15 +28,25 @@ public class Hooks {
         Utils.testCount++;
     }
 
+    @AfterStep
+    public void getResultStatus(Scenario scenario) throws IOException {
+        if(scenario.isFailed()) {
+            String screenshotPath = Utils.getScreenshot(driver, scenario.getName().replace(" ", "_"));
+            extentTest.log(LogStatus.FAIL, scenario.getName()+"\n"
+                    +extentTest.addScreenCapture(screenshotPath));;
+        }
+    }
+
     @After
     public void closeObject() {
         reports.endTest(extentTest);
         reports.flush();
     }
 
+
     @AfterAll
     public static void closeBrowser() {
-        delay(3);
+        delay(2);
         DriverSingleton.closeObjectInstance();
     }
 
@@ -53,7 +64,4 @@ public class Hooks {
         js.executeScript("window.scrollBy(0,"+vertical+")");
     }
 
-
 }
-
-
